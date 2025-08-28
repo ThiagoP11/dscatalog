9924411de418ebd3872ec29aab1b3789960fc49a
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-08-28 19:51:26 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscatolg/DscatolgApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatolgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatolgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatolg/DscatolgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatolgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
