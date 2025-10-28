0aaf90752767750b649ab65bddbf756ef743b1ea
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-10-28 19:50:52 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscatalog.dto;
+
+public class UserInsertDTO extends UserDTO{
+
+    private String password;
+
+    public UserInsertDTO() {
+        super();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -15,10 +15,11 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)//vai força que sempre que buscar um usuario do banco, vai trazer os roles(perfis) dele
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
